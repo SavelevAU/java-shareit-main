abaa1d4114ba434888d231dcfe39c54ce72f1522
Savelev_AU <andreisavelv@yandex.ru>
2025-06-30 13:31:53 +0300

    исправление ошибок постмана, добавление валидации юзера

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,8 +14,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserDto createUser(@RequestBody UserDto userDto) {
-        return userService.createUser(userDto);
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.createUser(userDto));
     }
 
     @PatchMapping("/{userId}")
