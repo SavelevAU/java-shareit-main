9350eb16b4feae1fcf6c4fcdadebeff3690c397a
Savelev_AU <andreisavelv@yandex.ru>
2025-06-30 11:48:18 +0300

    добавление пустых строк между методами сервисов

--- src/main/java/ru/practicum/shareit/request/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestService.java
@@ -5,8 +5,13 @@ import ru.practicum.shareit.request.dto.ItemRequestDto;
 import java.util.List;
 
 public interface ItemRequestService {
+
     ItemRequestDto createItemRequest(Long userId, ItemRequestDto dto);
+
     List<ItemRequestDto> getAllRequestsByUser(Long userId);
+
     List<ItemRequestDto> getAllRequests();
+
     ItemRequestDto getRequestById(Long requestId);
+
 }
--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -3,9 +3,15 @@ package ru.practicum.shareit.user;
 import java.util.List;
 
 public interface UserService {
+
     UserDto createUser(UserDto userDto);
+
     UserDto updateUser(Long userId, UserDto userDto);
+
     void deleteUser(Long userId);
+
     UserDto getUserById(Long userId);
+
     List<UserDto> getAllUsers();
+    
 }
